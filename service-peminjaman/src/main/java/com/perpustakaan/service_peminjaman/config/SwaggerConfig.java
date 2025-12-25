5d7808274c1ee0d142489f8aeb89126aa6333e47
erlaaaand <erlandagsya2@gmail.com>
2025-12-25 23:00:15 +0700

    integration swagger

---
+++ service-peminjaman/src/main/java/com/perpustakaan/service_peminjaman/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.perpustakaan.service_peminjaman.config; // Sesuaikan package
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title(applicationName.toUpperCase() + " API Documentation")
+                        .version("1.0")
+                        .description("API documentation for " + applicationName)
+                        .contact(new Contact()
+                                .name("Perpustakaan Team")
+                                .email("team@perpustakaan.com")))
+                .servers(List.of(
+                        new Server()
+                                .url("http://localhost:" + serverPort)
+                                .description("Local Development Server"),
+                        new Server()
+                                .url("http://localhost:8080")
+                                .description("API Gateway")
+                ));
+    }
+}
