e8149123af949f4045678655ebd0e787932eafac
erlaaaand <erlandagsya2@gmail.com>
2025-12-26 01:13:39 +0700

    update dto pengembalian

--- service-pengembalian/src/main/java/com/perpustakaan/service_pengembalian/dto/PengembalianRequest.java
+++ service-pengembalian/src/main/java/com/perpustakaan/service_pengembalian/dto/PengembalianRequest.java
@@ -1,10 +1,22 @@
 package com.perpustakaan.service_pengembalian.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class PengembalianRequest {
+    
+    @NotNull(message = "ID Peminjaman tidak boleh kosong")
     private Long peminjamanId;
+    
+    @NotBlank(message = "Tanggal dikembalikan tidak boleh kosong")
     private String tanggalDikembalikan;
+    
+    @Min(value = 0, message = "Keterlambatan tidak boleh kurang dari 0")
     private int terlambat;
+    
+    @Min(value = 0, message = "Denda tidak boleh kurang dari 0")
     private double denda;
 }
