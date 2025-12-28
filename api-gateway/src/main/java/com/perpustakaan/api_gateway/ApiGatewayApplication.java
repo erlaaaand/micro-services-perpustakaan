4808eaecce367be2b6716ae5c1c7f3990cf99b55
erlaaaand <erlandagsya2@gmail.com>
2025-12-28 20:56:44 +0700

    fix apigateway application

--- api-gateway/src/main/java/com/perpustakaan/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/perpustakaan/api_gateway/ApiGatewayApplication.java
@@ -2,6 +2,9 @@ package com.perpustakaan.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
@@ -10,4 +13,18 @@ public class ApiGatewayApplication {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
+	// Konfigurasi Global CORS untuk Gateway MVC
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**") // Berlaku untuk semua route
+						.allowedOrigins("*") // Sesuaikan dengan domain frontend Anda untuk produksi
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*")
+						.maxAge(3600);
+			}
+		};
+	}
 }
