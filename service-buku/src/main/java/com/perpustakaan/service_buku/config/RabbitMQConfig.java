6068d7031c07e193ed2b4f4c53aebfd2c2dfd99d
erlaaaand <erlandagsya2@gmail.com>
2026-01-03 15:50:01 +0700

    feat rabbitmq config file

---
+++ service-buku/src/main/java/com/perpustakaan/service_buku/config/RabbitMQConfig.java
@@ -0,0 +1,50 @@
+package com.perpustakaan.service_buku.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${perpustakaan.rabbitmq.queue}")
+    private String queueName;
+
+    @Value("${perpustakaan.rabbitmq.exchange}")
+    private String exchangeName;
+
+    @Value("${perpustakaan.rabbitmq.routing-key}")
+    private String routingKey;
+
+    @Bean
+    public Queue queue() {
+        return new Queue(queueName, true);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(exchangeName);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(routingKey);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(jsonMessageConverter());
+        return template;
+    }
+}
